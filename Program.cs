0671ba66296ed1a612aa591ab4dd944c26d45a4a
Konstantin <kostya-krisanov@mail.ru>
2022-08-31 23:45:50 +0300

    Напишите программу, которая на выход принемает два числаи выдает, какое больше, а какое меньше

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿Console.WriteLine("Input first number ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Input sekond number ");
+int b = Convert.ToInt32(Console.ReadLine());
+if (a > b)
+{
+   int Max = a;
+   Console.Write("Max =  ");
+   Console.WriteLine(Max);
+   int Min = b;
+   Console.Write("Min =  ");
+   Console.WriteLine(Min);
+}
+   else
+   {
+    int Max = b;
+    Console.Write("Max =  ");
+    Console.WriteLine(Max);
+     int Min = a;
+     Console.Write("Min =  ");
+   Console.WriteLine(Min);
+   }
