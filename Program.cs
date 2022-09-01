3f9e82545d52ee1823cc3424792cb702bfbaa0fa
Konstantin <kostya-krisanov@mail.ru>
2022-09-01 15:00:49 +0300

    Напишите программу, которая принемает на вход три числа и выдает максимальное из них

--- Program.cs
+++ Program.cs
@@ -1,22 +1,39 @@
-﻿Console.WriteLine("Input first number ");
+﻿//Console.WriteLine("Input first number ");
+//int a = Convert.ToInt32(Console.ReadLine());
+//Console.WriteLine("Input sekond number ");
+//int b = Convert.ToInt32(Console.ReadLine());
+//if (a > b)
+//{
+//   int Max = a;
+//   Console.Write("Max =  ");
+//   Console.WriteLine(Max);
+//   int Min = b;
+//   Console.Write("Min =  ");
+//   Console.WriteLine(Min);
+//}
+//   else
+//   {
+ //   int Max = b;
+ //   Console.Write("Max =  ");
+ //   Console.WriteLine(Max);
+//     int Min = a;
+//     Console.Write("Min =  ");
+//   Console.WriteLine(Min);
+//   }
+
+
+Console.WriteLine("Input number one ");
 int a = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Input sekond number ");
+Console.WriteLine("Input number two ");
 int b = Convert.ToInt32(Console.ReadLine());
-if (a > b)
-{
-   int Max = a;
-   Console.Write("Max =  ");
-   Console.WriteLine(Max);
-   int Min = b;
-   Console.Write("Min =  ");
-   Console.WriteLine(Min);
-}
-   else
-   {
-    int Max = b;
-    Console.Write("Max =  ");
+Console.WriteLine("Input number three ");
+int c = Convert.ToInt32(Console.ReadLine());
+int Max = a;
+while (a > Max)
+   Max = a;
+while (b > Max)
+    Max = b;
+while (c > Max)
+    Max = c; 
+    Console.Write("Max =   ");
     Console.WriteLine(Max);
-     int Min = a;
-     Console.Write("Min =  ");
-   Console.WriteLine(Min);
-   }
