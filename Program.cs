e921ed36d47dde88fa386e6717d4e837f758b1b6
Konstantin <kostya-krisanov@mail.ru>
2022-09-03 23:45:21 +0300

    Узнал разницу void от int; решил задачу: Напишите программу , которая выводит случайное число из отрезка [10;99] и показывает наибольшую цифру числа.

--- Program.cs
+++ Program.cs
@@ -47,11 +47,11 @@
 //        Console.WriteLine("number no even ");  
    
 
-Console.WriteLine("Input number ");
-int a = Convert.ToInt32(Console.ReadLine());
-int i = 2; 
-while (i <= a)
-{
-Console.Write(i + " ");
- i = i + 2 ;
-}
+// Console.WriteLine("Input number ");
+// int a = Convert.ToInt32(Console.ReadLine());
+// int i = 2; 
+// while (i <= a)
+// {
+// Console.Write(i + " ");
+//  i = i + 2 ;
+// }
---
+++ Seminar002/Program.cs
@@ -0,0 +1,37 @@
+﻿// Отличие void 
+// void f(ref int n) //Когда мы передаем процедуру без ref мы передаем копию этой переменной, а что бы обратиться к основной переменной нужно указывать ref.
+// {
+//     n += 5;
+// } 
+// int n = 5;
+// f(ref n);
+// Console.Write(n);   // т.е. с реф результат будет 10, а без 5.
+
+// int f(int n)
+// {
+//     return n * 2;    // return возвращает какое то значение и завершает в этом же месте работу функции
+// }
+// int n = 10;
+// Console.Write(f(n));
+
+// ЗАДАЧИ С СЕМИНАРА
+
+int GenerateRandomNumbers()
+{
+    int n = new Random().Next(10, 100);   // генерирует случайное число в заданном диапозоне
+    return n;
+}
+void ComprasisNumbers()
+{
+    int n = GenerateRandomNumbers();
+    Console.WriteLine(n);
+    int a = n / 10;                       // а это первая цыфра числа , а b втораяю
+    int b = n % 10;
+    if (a > b)
+        Console.WriteLine(a);
+     else if (a < b)
+         Console.WriteLine(b);
+     else
+          Console.WriteLine("=");        
+}
+ComprasisNumbers();
